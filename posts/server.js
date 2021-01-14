1f50a59ff4e27e0340d582911b7bdf3d34ab732c
Yvette <nchombuayvta@gmil.com>
2021-01-14 13:58:52 +0100

    post created

--- posts/server.js
+++ posts/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const { randomBytes } = require("crypto");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 const posts = {};
 
